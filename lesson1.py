031078961c4399471fa8d63884f167e17dd81897
Mete Eren Çelik <meteerencelik@hotmail.com>
2024-11-19 19:20:53 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+#cmd + opens python file
+from idlelib.run import MyHandler
+from lib2to3.fixes.fix_asserts import NAMES
+
+print("hello ai")
+
+# hastag allos to you as comment
+
+#cmd + 1 opens project tab
+#cmd + ö  for setting
+# terminal = option + F12
+#ls see your files and where you are
+
+
+x = "kemal"
+y = "Eren"
+z = "Mete"
+print(x,y ,z)
+
+myname = "Eren"
+print("hello " + myname)
